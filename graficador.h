5cbbf8a82f30266f1d8b29c3ffcfbd202c921bbd
Mat. Hdez. <mmateo95@protonmail.com>
2026-02-25 17:30:23 -0600

    Add 3D mode support to MyGraphCanvas
    Added methods to support 3D mode in MyGraphCanvas.

--- graficador.h
+++ graficador.h
@@ -9,6 +9,17 @@ class MyGraphCanvas : public wxPanel {
 public:
     MyGraphCanvas(wxWindow* parent, wxPoint pos, wxSize size);
     void OnPaint(wxPaintEvent& event);
+
+    // Nuevos métodos para la GUI
+    void SetModo3D(bool activar3D);
+    bool Is3D() const { return modo3D; }
+
+private:
+    bool modo3D = false; // Estado actual del canvas
+
+    // Separamos la lógica para mayor orden
+    void Dibujar2D(wxGraphicsContext* gc, int w, int h);
+    void Dibujar3D(wxGraphicsContext* gc, int w, int h);
 };
 
 #endif
